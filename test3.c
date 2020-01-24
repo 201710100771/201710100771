0a1012aaed8cdae17bb1079162f716c0b44030e3
201710100771 <60249488+201710100771@users.noreply.github.com>
2020-01-24 10:53:55 +0530

    Add files via upload

---
+++ test3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ void main()
+{
+ char choice;
+ printf("enter the choice of car\n");
+ scanf(" %c",&choice);
+ if (choice=='m')
+ printf("mersedez benz\n");
+ else if(choice=='p')
+ printf("porsche\n");
+ else if(choice=='j')
+ printf("jaguar\n");
+ else
+ printf("travel by bus\n");
+}
